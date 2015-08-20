737ab55ea44e5c951e71720a32901523013997bd
Hai Bui <hoanghaibui2003@gmail.com>
2015-08-20 14:40:27 +0700

    Tag new game?

--- app/home-page/homePageCtrl.js
+++ app/home-page/homePageCtrl.js
@@ -86,26 +86,113 @@ angular.module('app').controller('app.home-page.homePageCtrl', ['$scope', '$docu
     //// check it's possiable for move or merge  follow direct
     // merge if same value follow direct
     //move up when possiable follow direct
-    function getAllPointsHaveValue() {
-        var i, points = $scope.points, length = points.length;
-        for (i = 0; i < length; i++) {
-            if (points[i].value !== 0) {
+    var direct = 'top';
+    function getNextPositionFollowCurrentPostion(currentPosition, j) {
+        switch (direct) {
+            case 'top':
+                return currentPosition - j * 4;
+            case 'right':
+                if (currentPosition + j * 1 >15 || currentPosition + j * 1 === 12 || currentPosition + j * 1 === 8 || currentPosition + j * 1 === 4) {
+                    return -1;
+                }
+                return currentPosition + j * 1;
+            case 'bottom':
+                if (currentPosition + j * 4 > 15) {
+                    return -1;
+                }
+                return currentPosition + j * 4;
+            default:
+                // case 'left':
+                if (currentPosition - j * 1 === 11 || currentPosition - j * 1 === 7 || currentPosition - j * 1 === 3) {
+                    return -1;
+                }
+                return currentPosition - j * 1;
+        }
+
+    }
+
+    function getNextPositionFollowDirect(i) {
+        switch (direct) {
+            case "top":
+                if (i === -1) {
+                    return 0;
+                }
+                return i + 1;
+            case "right":
+                if (i === -1) {
+                    return 15;
+                }
+                if (i - 4 < 0) {
+                    return 11 + i;
+                }
+                return i - 4;
+            case "bottom":
+                if (i === -1) {
+                    return 15;
+                }
+                if (i - 4 < 0) {
+                    return 11 + i;
+                }
+                return i - 4;
+            default:
+                if (i === -1) {
+                    return 0;
+                }
+                return i + 1;
+        }
+    }
+    function run1() {
+        var i = 0, points = $scope.points, length = points.length, currentPostion = -1;
+        // check all points follow direct
+        //ex: direct:up => start =0
+        while (i < 16) {
+            currentPostion = getNextPositionFollowDirect(currentPostion);
+            if (points[currentPostion].value !== 0) {
+                console.log('currentPostion: ' + currentPostion);
+                var tempPosition = -1;
+                for (var j = 1; j < 4; j++) {
+                    //calculate the next point follow direct
+                    //ex: position:15; direct:up -> next position: 11
+                    var nextPosition = getNextPositionFollowCurrentPostion(currentPostion, j);
+                    if (nextPosition < 0 ) {
+                        break;
+                    }
+              //      console.log('nextPosition: ' + nextPosition);
+                    // merge if same value follow direct
+                    if (points[currentPostion].value === points[nextPosition].value) {
+
+                    }
+                    if (points[nextPosition].value !== 0) {
+                        break;
+                    }
+                    //move
+                    if (points[nextPosition].value === 0) {
+                        tempPosition = nextPosition;
                 
+                    }
+                }
+                if (tempPosition !== -1) {
+                    points[tempPosition].value = points[currentPostion].value;
+                    points[currentPostion].value = 0;
+                }
             }
+            i++;
         }
     }
 
+
+
     function checkPossiableForMoveOrMerge(direct) {
-        
+
     }
     function mergeValue(direct) {
-        
+
     }
     function move() {
         //
     }
     function arrowUp() {
-        var i, points = $scope.points, length = points.length,tempPosition;
+        var i, points = $scope.points, length = points.length, tempPosition;
         for (i = 4; i < length; i++) {
             tempPosition = undefined;
             for (var t = i; t > 0; t = t - 4) {
@@ -135,7 +222,7 @@ angular.module('app').controller('app.home-page.homePageCtrl', ['$scope', '$docu
                 points[i].value = 0;
             }
             //case equal and not calculated yet
-           
+
 
         }
     }
@@ -164,33 +251,39 @@ angular.module('app').controller('app.home-page.homePageCtrl', ['$scope', '$docu
         tempPoints = 0;
         createNewPoints();
         newRandom();
-        newRandom();
+        //  newRandom();
         reCheckClassValues();
     }
     $document.on('keydown', function (event) {
         console.log(event.keyCode);
+        var matched = false;
         switch (event.keyCode) {
             case 37:
-                arrowLeft();
+                direct = "left";
+                matched = true;
                 break;
             case 38:
-                if (!canArrowUp()) {
-                    return;
-                }
-                arrowUp();
+                direct = "top";
+                matched = true;
                 break;
-            case 39: arrowRight();
+            case 39:
+                direct = "right";
+                matched = true;
                 break;
-            case 40: arrowDown();
+            case 40:
+                direct = "bottom";
+                matched = true;
                 break;
-            default:
         }
-        $scope.score += tempPoints;
-        tempPoints = 0;
-        newRandom();
-        reCheckClassValues();
-        clearCalculatedPoint();
-        $scope.$digest();
+        if (matched) {
+            run1();
+            //$scope.score += tempPoints;
+            //tempPoints = 0;
+            //newRandom();
+            reCheckClassValues();
+            //clearCalculatedPoint();
+            $scope.$digest();
+        }
     });
 
 
